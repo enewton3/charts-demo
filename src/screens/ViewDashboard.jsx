8cbeaae583c1afc37b4104bc7bc83b7ab802daea
Evyn Newton <newton.evyn@gmail.com>
2021-05-10 18:08:20 -0400

    view grid and select menu

--- src/screens/ViewDashboard.jsx
+++ src/screens/ViewDashboard.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core";
+import React, { useState } from "react";
+import { Button, makeStyles, Menu } from "@material-ui/core";
 import DashboardSelect from "../components/DashboardSelect";
 import ViewDashboardGrid from "../components/ViewDashboardGrid";
 
@@ -14,6 +14,13 @@ const useStyles = makeStyles((theme) => ({
   },
   select: {
     width: "25%",
+    position: "absolute",
+    right: 0,
+  },
+  grid: {
+    border: "2px solid red",
+    width: "90vw",
+    height: "100vh",
   },
 }));
 
@@ -23,18 +30,42 @@ export default function ViewDashboard({
   handleDashboardSelect,
 }) {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleMenuClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className={classes.viewContainer}>
       <div className={classes.select}>
-        <DashboardSelect
-          dashboards={dashboards}
-          handleDashboardSelect={handleDashboardSelect}
-          currentDashboard={currentDashboard}
-        />
+        <Button
+          onClick={handleMenuClick}
+          aria-haspopup="true"
+          color="primary"
+          aria-controls="menu"
+        >
+          Dashboard Select
+        </Button>
+        <Menu
+          id="menu"
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          keepMounted
+          open={Boolean(anchorEl)}
+          anchorOrigin={{ vertical: "bottom", horizontal: "" }}
+        >
+          <DashboardSelect
+            dashboards={dashboards}
+            handleDashboardSelect={handleDashboardSelect}
+            currentDashboard={currentDashboard}
+          />
+        </Menu>
       </div>
 
-      <div>
+      <div className={classes.grid}>
         <ViewDashboardGrid currentDashboard={currentDashboard} />
       </div>
     </div>
